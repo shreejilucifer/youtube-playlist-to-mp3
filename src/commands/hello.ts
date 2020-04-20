e46be445a1bf0c4fe3490787b62a4475e26fb734
Shreeji Pedhadiya <shreejilucifer@gmail.com>
2020-04-20 19:27:46 +0530

    Single Video Downloader

--- src/commands/hello.ts
+++ src/commands/hello.ts
@@ -1,8 +1,12 @@
-import { logInfo } from '../utils';
+import { logInfo, logError, logDetail } from '../utils';
 import { prompt as ask } from 'inquirer';
 import * as yargs from 'yargs';
+import * as ytdl from 'ytdl-core';
+import * as fs from 'fs';
+//@ts-ignore
+import * as hbjs from 'handbrake-js';
 
-export type Params = { name?: string };
+export type Params = { name?: string; url?: string };
 
 export const command = 'hello';
 export const desc = `Let's get to know each other`;
@@ -19,10 +23,54 @@ async function askName(): Promise<string> {
 	return name;
 }
 
+async function askURL(): Promise<string> {
+	const { url } = await ask([
+		{
+			type: 'input',
+			name: 'url',
+			message: 'Enter Youtube URL:',
+		},
+	]);
+
+	return url;
+}
+
 export const builder: { [key: string]: yargs.Options } = {
 	name: { type: 'string', required: false, description: 'your name' },
+	url: { type: 'string', require: false, description: 'youtube url' },
 };
 
-export async function handler({ name }: Params) {
-	logInfo(`Oh, nice to meet you, ${name || (await askName())}!`);
+export async function handler(args: Params) {
+	// const name = args.name || (await askName());
+	const url = args.url || (await askURL());
+
+	// logInfo(`Oh, nice to meet you, ${name}!`);
+
+	const info = await ytdl.getBasicInfo(url);
+
+	logInfo(`Video Title: ${info.title}`);
+
+	const res = await ytdl(url);
+
+	res.pipe(fs.createWriteStream('myvideo.flv'));
+
+	res.on('data', () => {});
+
+	res.on('end', async () => {
+		console.log('Video Downloaded ! Starting HBJS');
+
+		await hbjs
+			.spawn({ input: 'myvideo.flv', output: 'something.mp3' })
+			.on('error', (err: Error) => {
+				// invalid user input, no video found etc
+				logError(err.message);
+			})
+			.on('progress', (progress: any) => {
+				console.log(
+					'Percent complete: %s, ETA: %s',
+					progress.percentComplete,
+					progress.eta
+				);
+			});
+	});
 }
